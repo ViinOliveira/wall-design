8ad29c1f92da29c18af6cc387d90b9a49ac9345d
ViinOliveiraa <vicavalcante023@gmail.com>
2025-05-05 19:45:58 -0300

    Atualização completa com imagem do hero

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-<script>
-  document.querySelector('.menu-toggle').addEventListener('click', function () {
+ document.querySelector('.menu-toggle').addEventListener('click', function () {
     document.querySelector('.menu').classList.toggle('show');
   });
-</script>
